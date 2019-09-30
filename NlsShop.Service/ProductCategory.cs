90036fa0572977b955d62b9810dd71032647e76b
lmthien96 <leminhthien18081996@gmail.com>
2019-09-30 14:29:30 +0700

    search by keyword product category list

--- NlsShop.Service/ProductCategory.cs
+++ NlsShop.Service/ProductCategory.cs
@@ -16,6 +16,8 @@ namespace NlsShop.Service
 
         IEnumerable<ProductCategory> GetAll();
 
+        IEnumerable<ProductCategory> GetAll(string keyword);
+
         IEnumerable<ProductCategory> GetAllByParentId(int parentId);
 
         ProductCategory GetById(int id);
@@ -47,6 +49,18 @@ namespace NlsShop.Service
         public IEnumerable<ProductCategory> GetAll()
         {
             return _productCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<ProductCategory> GetAll(string keyword)
+        {
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                return _productCategoryRepository.GetMulti(x => x.Name.Contains(keyword) || x.Description.Contains(keyword));
+            }
+            else
+            {
+                return _productCategoryRepository.GetAll();
+            }
         }
 
         public IEnumerable<ProductCategory> GetAllByParentId(int parentId)
